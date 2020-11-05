81a0e6c3ce2efab7e1c542238f0dc289640248d7
JamesGaier <james.gaier003@gmail.com>
2020-11-05 11:35:53 -0800

    player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D r2d;
+    public float speed;
+    public float jumpForce;
+    public bool isGrounded = false;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public Transform isGroundedChecker;
+    public float checkGroundRadius;
+    public LayerMask groundLayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        r2d = GetComponent<Rigidbody2D>();
+    }
+    void CheckIfGrounded()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+
+        isGrounded = (collider != null) ? true : false;
+    }
+    void Move()
+    {
+        r2d.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, r2d.velocity.y);
     }
 
+    void BetterJump()
+    {
+        if (r2d.velocity.y < 0)
+        {
+            r2d.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (r2d.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            r2d.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            r2d.velocity = new Vector2(r2d.velocity.x, jumpForce);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Jump();
+        BetterJump();
+        CheckIfGrounded();
     }
 }
