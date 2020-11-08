b4e7f43589ebaa9517935f9aedf507df0fcb315b
JamesGaier <james.gaier003@gmail.com>
2020-11-08 15:23:58 -0800

    Justin and I defined how platforms work and refactored the player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,87 +4,72 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D r2d;
-    public float speedX;
+    public float moveSpeed;
     public float jumpForce;
-    public bool isGrounded = false;
-    public bool hittingWall = false;
-    public float fallMultiplier = 2.5f;
-    public float lowJumpMultiplier = 2f;
-    public float rememberGroundedFor;
-    float lastTimeGrounded;
-    public Transform isGroundedChecker;
-    public float checkGroundRadius;
-    public LayerMask groundLayer;
-    // Start is called before the first frame update
-    void Start()
-    {
-        r2d = GetComponent<Rigidbody2D>();
-    }
-    void CheckIfGrounded()
-    {
-        Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+    public Transform GroundCheck;
+    public LayerMask groundObjects;
+    public float checkRadius;
 
-        if(collider != null)
-        {
-            isGrounded = true;
-        }
-        else
-        {
-            if(isGrounded)
-            {
-                lastTimeGrounded = Time.time;
-            }
-            isGrounded = false;
-        }
+    private bool isJumping = false;
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private float moveDirection;
+    private bool isGrounded;
 
-    }
-    void Move()
+    void Awake()
     {
-        if(!hittingWall)
-        {
-            r2d.velocity = new Vector2(Input.GetAxis("Horizontal") * speedX, r2d.velocity.y);
-
-        }
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void Jump()
+    void FixedUpdate()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && (isGrounded ||
-            Time.time - lastTimeGrounded <= rememberGroundedFor))
-        {
-            r2d.velocity = new Vector2(r2d.velocity.x, jumpForce);
-        }
+        isGrounded = Physics2D.OverlapCircle(GroundCheck.position, checkRadius, groundObjects);
 
-        if (r2d.velocity.y < 0)
-        {
-            r2d.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
-        }
-        else if (r2d.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
-        {
-            r2d.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
-        }
-    }
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        if(coll.gameObject.layer == 8 && !isGrounded && Input.GetAxis("Horizontal") != 0)
-        {
-            hittingWall = true;
-        }
-        else
-        {
-            hittingWall = false;
-        }
-    }
-    void OnCollisionExit2D(Collision2D coll)
-    {
-        hittingWall = false;
-    }
-    void Update()
-    {
+
 
         Move();
-        Jump();
-        CheckIfGrounded();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+
+        Animate();
+
+       
+    }
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+        if (isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+        }
+        isJumping = false;
+    }
+    private void Animate()
+    {
+        if (moveDirection > 0 && !facingRight)
+        {
+            FlipCharacter();
+        }
+        else if (moveDirection < 0 && facingRight)
+        {
+            FlipCharacter();
+        }
+    }
+    private void ProcessInputs()
+    {
+        moveDirection = Input.GetAxis("Horizontal");
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
+    }
+    private void FlipCharacter()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
     }
 }
