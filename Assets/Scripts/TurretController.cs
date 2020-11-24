d60fd8f7ad6338cfb2270bcfeae5feabbed7c0d3
JamesGaier <james.gaier003@gmail.com>
2020-11-24 12:40:50 -0800

    got turret movement working

--- Assets/Scripts/TurretController.cs
+++ Assets/Scripts/TurretController.cs
@@ -5,29 +5,36 @@ using UnityEngine;
 public class TurretController : MonoBehaviour
 {
 
-
     public Transform player;
-    public int[] angles;
+    public float fireCooldown;
+    public float turretTimeStep;
 
     private Rigidbody2D rb;
-    //private float currentTime = 0f;
+    private float currentTime = 0f;
+    private float timeSinceLastFire = 0f;
 
-   
+
+    float degToRad(float degrees)
+    {
+        return degrees * Mathf.Deg2Rad;
+    }
+
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector3 dir = player.position - transform.position;
         float rawAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-
-        int angIdx = 0;
-
-        
-
-        rb.rotation = rawAngle;
+        if (currentTime > turretTimeStep)
+        {
+            rb.rotation = Mathf.Round(rawAngle / 45) * 45;
+            currentTime = 0;
+        }
+        currentTime += Time.deltaTime;
+        timeSinceLastFire += Time.deltaTime;
     }
 }
