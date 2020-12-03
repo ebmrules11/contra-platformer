056bad60e5a8791c7f0f7a68b794d7d1e9d28c1a
Ethan <ebmrules@gmail.com>
2020-12-03 01:16:12 -0800

    Added Boss Mechanics to Variant

--- Assets/Scripts/BossMovement.cs
+++ Assets/Scripts/BossMovement.cs
@@ -13,6 +13,9 @@ public class BossMovement : MonoBehaviour
     public ProjectileController projectileController;
     public Transform bulletOriginT;
     public Transform player;
+
+    public GameObject playerOBJ;
+
     Vector3 posOffset = new Vector3 ();
     Vector3 tempPos = new Vector3 ();
     [SerializeField] float fireCooldown;
@@ -21,11 +24,28 @@ public class BossMovement : MonoBehaviour
     public bool facingRight;
     public Transform firepoint;
     public GameObject bulletPrefab;
+    public AudioSource boss_audio;
+    public AudioSource game_audio;
+    public AudioSource boss_dead_audio;
+    public AudioSource boss_after_dead_audio;
+    private bool audioChanged = false;
+
+    public float health = 150f;
+    public PlayerAnimation animation;
+
+    public bool isAlive;
+	public BoxCollider2D collider;
+	public BoxCollider2D colliderOnDeath;
    
 
 
     void Start()
     {
+        game_audio.Pause();
+        boss_audio.Play();
+        
+        
+        isAlive = true;
         posOffset = transform.position;
     }
 
@@ -33,6 +53,7 @@ public class BossMovement : MonoBehaviour
     void Update()
     {
  
+
         // Float up/down with a Sin()
         tempPos = posOffset;
         tempPos.y += Mathf.Sin (Time.fixedTime * Mathf.PI * frequency) * amplitude;
@@ -40,22 +61,62 @@ public class BossMovement : MonoBehaviour
 
         timeSinceLastFire += Time.deltaTime;
 
-        
-		if(timeSinceLastFire >= fireCooldown){
+        float distanceFromPlayer = Vector2.Distance(transform.position, playerOBJ.transform.position);
+
+       if(distanceFromPlayer < sensingDistance){
+			Vector3 dir = player.position - transform.position;
 			
-			fire();
-			timeSinceLastFire = 0f;
+			if(timeSinceLastFire >= fireCooldown){
+				fire();
+				timeSinceLastFire = 0f;
+			}
 		}
+		timeSinceLastFire += Time.deltaTime;
 
     }
     void fire(){
-		
-		
-		Vector3 dir = player.position - transform.position;
-		if(dir.magnitude < sensingDistance){
-			Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+
+
+
+		Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+	}
+
+    private void takeHit(){
+		health--;
+		//animation.takeHit();
+		if(health <= 0){
+			collider.enabled = false;
+			colliderOnDeath.enabled = true;
+			die();
 		}
 	}
+    void OnTriggerEnter2D(Collider2D otherCollider){
+		GameObject obj = otherCollider.gameObject;
+		if(obj.tag == "PlayerProjectile"){
+			takeHit();
+			GameObject.Destroy(obj);
+		}
+	}
+
+    private void die(){
+		isAlive = false;
+		animation.die();
+        boss_audio.Pause();
+        boss_dead_audio.Play();
+		StartCoroutine(destroyAfterDeath());
+        
+	}
+    
+    IEnumerator destroyAfterDeath(){
+		yield return new WaitUntil(() => animation.deathComplete);
+        
+        boss_after_dead_audio.Play();
+		GameObject.Destroy(this.gameObject);
+        
+        
+	}
+
+
     // Update is called once per frame
     void FixedUpdate()
     {
